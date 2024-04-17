d25252bf90a36cb120adf1a42421caf21cafc221
Surajit Dey <128603388+SURAJIT12003@users.noreply.github.com>
2024-04-17 11:28:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Valid Substring/valid-substring.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+
+            Solution ob = new Solution();
+            System.out.println(ob.findMaxLen(S));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int findMaxLen(String S) {
+        int ans =0;
+        int open =0;
+        int close =0;
+        for(int i=0;i<S.length();i++){
+            char c = S.charAt(i);
+            if(c=='('){
+                open++;
+            }
+            else{
+                close++;
+            }
+            
+            if(open==close){
+                ans= Math.max(ans,(open+close));
+            }
+            else if(close>open){
+                open=0;
+                close=0;
+            }
+        }
+        
+        open=0;
+        close =0;
+         for(int i=S.length()-1;i>=0;i--){
+            char c = S.charAt(i);
+            if(c=='('){
+                open++;
+            }
+            else{
+                close++;
+            }
+            
+            if(open==close){
+                ans= Math.max(ans,(open+close));
+            }
+            else if(close<open){
+                open=0;
+                close=0;
+            }
+        }
+        return ans;
+    }
+};
