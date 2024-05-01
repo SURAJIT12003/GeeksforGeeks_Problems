9f475054d778a5efb6d452c6c501d0f1ac771075
Surajit Dey <128603388+SURAJIT12003@users.noreply.github.com>
2024-05-01 21:09:44 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Arrange Consonants and Vowels/arrange-consonants-and-vowels.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    char data;
+    Node next;
+    
+    public Node(char data){
+        this.data = data;
+        next = null;
+    }
+}
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    Node head = null, tail = null;
+		    
+		    char head_c = sc.next().charAt(0);
+		    head = new Node(head_c);
+		    tail = head;
+		    
+		    while(n-- > 1){
+		        tail.next = new Node(sc.next().charAt(0));
+		        tail = tail.next;
+		    }
+		    
+		    Solution obj = new Solution();
+		    //show(head);
+		    show(obj.arrangeCV(head));
+		    
+		}
+	}
+	
+	public static void po(Object o){
+	    System.out.println(o);
+	}
+	
+	public static void show(Node head){
+        while(head != null){
+            System.out.print(head.data+" ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of node class is:
+class Node {
+    char data;
+    Node next;
+    
+    public Node(char data){
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution {
+    
+    public Node arrangeCV(Node head){
+        Node vowel = new Node('h');
+        Node temp = vowel;
+        Node cons  = new Node('k');
+        Node ptr  =cons;
+        
+        while(head!=null){
+            Node newNode = new Node(head.data);
+            if(head.data=='a' ||head.data=='e' ||head.data=='i' ||head.data=='o' ||head.data=='u'){
+                vowel.next = newNode;
+                vowel = newNode;
+            }
+            else{
+                cons.next = newNode;
+                cons = newNode;
+            }
+            head = head.next;
+        }
+        
+        vowel.next = ptr.next;
+        return temp.next;
+        
+        
+    }
+}
